797481293ce63f85b0d23515b92955b16bab247b
CharlesBronson72 <victor@dr-wiech.de>
2025-04-29 17:39:23 +0200

    neues testc++

---
+++ testc++.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    int *a = new int[10] {5, 4, 3, 2, 1, 0, 0, 0, 0, 0};
+    std::cout << "Adresse des Pointer a: " << a << std::endl;
+    for (int i = 0; i < 10; ++i)
+    {
+        std::cout << a[i] << " ";
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < 10; ++i)
+    {
+        a[i] = i;
+    }
+    for (int i = 0; i < 10; ++i)
+    {
+        std::cout << a[i] << " ";
+    }
+    std::cout << std::endl;
+    delete[] a;
+
+    std::cout << "Memory allocated and deallocated successfully." << std::endl;
+
+    int arr[10] = {0};
+    std::cout << "Adresse des arr[]: " << &arr << std::endl;
+    for (int i = 0; i < 10; ++i)
+    {
+        std::cout << "Adresse der arr[" << i << "] : " << &arr[i] << "\tInhalt : " << arr[i]    << "\n";
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < 10; ++i)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    for (int i = 0; i < 10; ++i)
+    {
+        arr[i] = i;
+    }
+    for (int i = 0; i < 10; ++i)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "Array initialized successfully." << std::endl;
+
+    return 0;
+}
